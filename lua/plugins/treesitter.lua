22d36ab9f63133e56fcd5d796536baf037d34cb8
肖文龙 <joel.xiao@dclingcloud.com>
2025-02-25 10:19:19 +0800

    feat: setting treesitter in plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,8 @@ return {
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
+        "vue",
+        "scss",
         "tsx",
         "typescript",
       })
