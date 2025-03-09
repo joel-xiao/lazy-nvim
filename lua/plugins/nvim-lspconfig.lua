d7cf936d592a28466976a3389b1a7bc0907e1fb3
肖文龙 <joel.xiao@dclingcloud.com>
2025-03-10 03:00:33 +0800

    feat: setting lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,39 +2,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      --
-      -- setup = {
-      --   defaults = {
-      --     capabilities = {
-      --       documentFormattingProvider = false,
-      --       documentRangeFormattingProvider = false,
-      --     },
-      --   },
-      -- },
 
-      -- 不再需要 ensure_installed 字段
       servers = {
         -- Vue3 (Volar)
         volar = {
-          -- 关闭 Volar 自带的模板格式化
           init_options = {
             vue = {
               hybridMode = false,
-              -- format = {
-              --   enable = false, -- 彻底关闭 Vue 模板格式化
-              -- },
             },
           },
-          -- -- 阻止自动注册格式化命令
-          -- commands = {
-          --   VueFormatDocument = false,
-          --   VueFormatSelection = false,
-          -- },
-          -- -- 关键配置：禁用所有格式化功能
-          -- capabilities = {
-          --   documentFormattingProvider = false,
-          --   documentRangeFormattingProvider = false,
-          -- },
         },
 
         -- HTML
