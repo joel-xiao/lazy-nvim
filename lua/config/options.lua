98d53983ee400cf50d6882327dd649da9235f7c4
肖文龙 <joel.xiao@dclingcloud.com>
2025-04-17 13:19:23 +0800

    feat: setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,17 +3,18 @@
 -- Add any additional options here
 
 -- This file is automatically loaded by plugins.core
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
 
 -- LazyVim auto format
 vim.g.autoformat = false
+vim.b.autoformat = false
 
 -- if the completion engine supports the AI source,
 -- use that instead of inline suggestions
-vim.g.ai_cmp = false
+-- vim.g.ai_cmp = false
 
 -- set to `true` to follow the main branch
 -- you need to have a working rust toolchain to build the plugin
 -- in this case.
-vim.g.lazyvim_blink_main = true
+-- vim.g.lazyvim_blink_main = true
