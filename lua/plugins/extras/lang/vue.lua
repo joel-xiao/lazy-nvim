e3aa82493aea1750f3a567324267b9947f4446f6
肖文龙 <joel.xiao@dclingcloud.com>
2025-09-10 16:03:52 +0800

    feat: vue lsp config update

--- lua/plugins/extras/lang/vue.lua
+++ lua/plugins/extras/lang/vue.lua
@@ -1,29 +1,43 @@
 
 return {
-  -- 配置 LSP 插件，添加 volar 和 tsserver 配置
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- Vue 3 使用 volar
         volar = {
-          enabled = true,  -- 启用 Vue 3 项目使用 volar
+          enabled = true,
+          filetypes = { "vue" },
           init_options = {
             vue = {
-              hybridMode = true,  -- 启用 hybridMode 支持 Vue 的 <script> 和 <template>
-            },
+              target = nil,
+              hybridMode = true,
+              optionsApi = {
+                enabled = true
+              }
+            }
           },
+          settings = {
+            vue = {
+              javascript = {
+                enabled = true
+              },
+              -- typescript = {
+              --   enabled = false
+              -- },
+              template = {
+                compilerOptions = {
+                  compatConfig = {
+                    MODE = 3
+                  }
+                }
+              }
+            }
+          }
         },
 
-        -- Vue 2 使用 vls
-        vls = {
-          enabled = true,  -- 启用 Vue Language Server
-        },
-
-        -- 确保 tsserver 插件启用，支持 JavaScript 和 TypeScript 提示
-        tsserver = {
-          enabled = true,  -- 启用 tsserver 插件
-        },
+        -- tsserver = {
+        --   enabled = false
+        -- }
       },
     },
   },
