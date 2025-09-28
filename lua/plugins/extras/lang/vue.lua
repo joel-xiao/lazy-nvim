fa8d931979ee10f4cd977fcedbe5f0bc28653668
肖文龙 <joel.xiao@dclingcloud.com>
2025-09-28 20:51:27 +0800

    feat: vue lsp config update

--- lua/plugins/extras/lang/vue.lua
+++ lua/plugins/extras/lang/vue.lua
@@ -1,43 +1,33 @@
-
+-- npm install -g @vue/language-server
+-- stylua: ignore
 return {
+  -- Vue 3 / Volar / @vue/language-server 配置
   {
     "neovim/nvim-lspconfig",
+    -- @class PluginLspOpts
     opts = {
+      -- @type lspconfig.options
       servers = {
-        volar = {
-          enabled = true,
+        vuels = {
           filetypes = { "vue" },
           init_options = {
             vue = {
               target = nil,
               hybridMode = true,
-              optionsApi = {
-                enabled = true
-              }
-            }
+              optionsApi = { enabled = true },
+            },
           },
           settings = {
             vue = {
-              javascript = {
-                enabled = true
-              },
-              -- typescript = {
-              --   enabled = false
-              -- },
+              javascript = { enabled = true },
               template = {
-                compilerOptions = {
-                  compatConfig = {
-                    MODE = 3
-                  }
-                }
-              }
-            }
-          }
+                compilerOptions = { compatConfig = { MODE = 3 } },
+              },
+            },
+          },
+          -- 指定全局安装的 @vue/language-server 路径
+          cmd = { "vue-language-server", "--stdio" },
         },
-
-        -- tsserver = {
-        --   enabled = false
-        -- }
       },
     },
   },
