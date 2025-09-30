59765c8a4ab774a474252b237fa78a67c6122572
肖文龙 <joel.xiao@dclingcloud.com>
2025-09-30 17:41:02 +0800

    feat: vue lsp config update

--- lua/plugins/extras/lang/vue.lua
+++ lua/plugins/extras/lang/vue.lua
@@ -19,30 +19,6 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        vue_ls = {
-          -- 确保 vue_ls 能找到 TypeScript 依赖
-          init_options = {
-            typescript = {
-              serverPath = vim.fn.stdpath("data") .. "/lsp_servers/vtsls/node_modules/typescript/lib",
-            },
-          },
-          -- 让 vue_ls 处理 Vue 文件的文档高亮
-          on_attach = function(client, bufnr)
-            if client.server_capabilities.documentHighlightProvider then
-              local group = vim.api.nvim_create_augroup("vue_ls_highlight", { clear = true })
-              vim.api.nvim_create_autocmd("CursorHold", {
-                buffer = bufnr,
-                group = group,
-                callback = vim.lsp.buf.document_highlight,
-              })
-              vim.api.nvim_create_autocmd("CursorMoved", {
-                buffer = bufnr,
-                group = group,
-                callback = vim.lsp.buf.clear_references,
-              })
-            end
-          end,
-        },
         vtsls = {
           -- 为 vtsls 添加 on_attach 隔离 Vue 文件
           on_attach = function(client, bufnr)
