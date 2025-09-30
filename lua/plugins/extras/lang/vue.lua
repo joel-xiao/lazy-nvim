aa54303984a1f666658b65f2ac3623403369ae7b
肖文龙 <joel.xiao@dclingcloud.com>
2025-09-30 16:10:14 +0800

    feat: vue lsp config update

--- lua/plugins/extras/lang/vue.lua
+++ lua/plugins/extras/lang/vue.lua
@@ -1,32 +1,70 @@
--- npm install -g @vue/language-server
--- stylua: ignore
 return {
-  -- Vue 3 / Volar / @vue/language-server 配置
   {
     "neovim/nvim-lspconfig",
-    -- @class PluginLspOpts
     opts = {
-      -- @type lspconfig.options
       servers = {
-        vuels = {
-          filetypes = { "vue", 'javascript', 'javascriptreact' },
+        -- Volar 用于 Vue 文件
+        volar = {
+          filetypes = { "vue" },
           init_options = {
-            vue = {
-              target = nil,
-              hybridMode = true,
-              optionsApi = { enabled = true },
-            },
+            vue = { hybridMode = true }, -- 必须开启，处理 template + script
+          },
+          on_attach = function(client, bufnr)
+            -- 高亮光标下符号
+            if client.server_capabilities.documentHighlightProvider then
+              local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+              vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+              vim.api.nvim_create_autocmd({ "CursorHold" }, {
+                group = group,
+                buffer = bufnr,
+                callback = vim.lsp.buf.document_highlight,
+              })
+              vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+                group = group,
+                buffer = bufnr,
+                callback = vim.lsp.buf.clear_references,
+              })
+            end
+          end,
+        },
+
+        -- vtsls 用于 TS/JS 文件
+        vtsls = {
+          filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+          init_options = {
+            hostInfo = "neovim",
           },
           settings = {
-            vue = {
-              javascript = { enabled = true },
-              template = {
-                compilerOptions = { compatConfig = { MODE = 3 } },
-              },
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+              enableMoveToFileCodeAction = true,
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = { completeFunctionCalls = true },
+            },
+            javascript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = { completeFunctionCalls = true },
             },
           },
-          -- 指定全局安装的 @vue/language-server 路径
-          cmd = { "vue-language-server", "--stdio" },
+          on_attach = function(client, bufnr)
+            -- 高亮光标下符号
+            if client.server_capabilities.documentHighlightProvider then
+              local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+              vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+              vim.api.nvim_create_autocmd({ "CursorHold" }, {
+                group = group,
+                buffer = bufnr,
+                callback = vim.lsp.buf.document_highlight,
+              })
+              vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+                group = group,
+                buffer = bufnr,
+                callback = vim.lsp.buf.clear_references,
+              })
+            end
+          end,
         },
       },
     },
