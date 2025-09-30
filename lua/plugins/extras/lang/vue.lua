af89451e97c267199b7ffc8a55f7afe04709b376
肖文龙 <joel.xiao@dclingcloud.com>
2025-09-30 17:17:27 +0800

    feat: vue lsp config update

--- lua/plugins/extras/lang/vue.lua
+++ lua/plugins/extras/lang/vue.lua
@@ -1,72 +1,44 @@
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "vue",
+      root = { "vue.config.js" },
+    })
+  end,
+
+  -- depends on the typescript extra
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "vue", "css" } },
+  },
+
+  -- Add LSP servers
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- Volar 用于 Vue 文件
-        volar = {
-          filetypes = { "vue" },
-          init_options = {
-            vue = { hybridMode = true }, -- 必须开启，处理 template + script
-          },
-          on_attach = function(client, bufnr)
-            -- 高亮光标下符号
-            if client.server_capabilities.documentHighlightProvider then
-              local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
-              vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-              vim.api.nvim_create_autocmd({ "CursorHold" }, {
-                group = group,
-                buffer = bufnr,
-                callback = vim.lsp.buf.document_highlight,
-              })
-              vim.api.nvim_create_autocmd({ "CursorMoved" }, {
-                group = group,
-                buffer = bufnr,
-                callback = vim.lsp.buf.clear_references,
-              })
-            end
-          end,
-        },
-
-        -- vtsls 用于 TS/JS 文件
-        vtsls = {
-          filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-          init_options = {
-            hostInfo = "neovim",
-          },
-          settings = {
-            vtsls = {
-              autoUseWorkspaceTsdk = true,
-              enableMoveToFileCodeAction = true,
-            },
-            typescript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              suggest = { completeFunctionCalls = true },
-            },
-            javascript = {
-              updateImportsOnFileMove = { enabled = "always" },
-              suggest = { completeFunctionCalls = true },
-            },
-          },
-          on_attach = function(client, bufnr)
-            -- 高亮光标下符号
-            if client.server_capabilities.documentHighlightProvider then
-              local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
-              vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-              vim.api.nvim_create_autocmd({ "CursorHold" }, {
-                group = group,
-                buffer = bufnr,
-                callback = vim.lsp.buf.document_highlight,
-              })
-              vim.api.nvim_create_autocmd({ "CursorMoved" }, {
-                group = group,
-                buffer = bufnr,
-                callback = vim.lsp.buf.clear_references,
-              })
-            end
-          end,
-        },
+        vue_ls = {},
+        vtsls = {},
       },
     },
+  },
+
+  -- Configure tsserver plugin
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
   },
 }
